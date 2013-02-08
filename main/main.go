caacc06fd1e62060de2d38ccde00e2dbdde8219f
freedomfita <jacobsherin2008@u.northwestern.edu>
2013-02-07 23:38:25 -0600

    New Main, lots of changes

--- main/main.go
+++ main/main.go
@@ -9,6 +9,10 @@ import (
     "net/http"
     "net/rpc"
     "time"
+    "os"
+    "bufio"
+    "strings"
+    "bytes"
 )
 
 import (
@@ -20,24 +24,67 @@ func main() {
     // By default, Go seeds its RNG with 1. This would cause every program to
     // generate the same sequence of IDs.
     rand.Seed(time.Now().UnixNano())
-
-    // Get the bind and connect connection strings from command-line arguments.
+	    // Get the bind and connect connection strings from command-line arguments.
     flag.Parse()
-    args := flag.Args()
-    if len(args) != 2 {
-        log.Fatal("Must be invoked with exactly two arguments!\n")
+    //args := flag.Args()
+    log.Printf("\nType q to quit\nFormat of Run\nrun str str")
+    
+    
+    for true {
+	    reader := bufio.NewReader(os.Stdin)
+	    input,_:= reader.ReadString('\n')
+	    //input includes both a carriage return and newline, trim whitespace
+	    input = strings.TrimSpace(input)
+	    /*l := len(input)
+	    for i:=0; i<l; i++ {
+	    	log.Printf("%d", input[i])}
+	    	*/
+	    
+	    arg_s := strings.Split(input, " ")
+	    length := len(arg_s)
+	    log.Printf("there were %d\n", length)
+	    for i := 0; i < length; i++ {
+	    	log.Printf("number %d: %s", i, arg_s[i])
+	    }
+	    if arg_s[0][0] == 113 { //ASCII for "q"
+	    	return
+	    } else{
+	    	log.Printf("Command/s unknown.")}
+	    
     }
+    
+}
+
+func chan_test(msg string, c chan string) {
+	for i:=0; ; i++ {
+		c<- fmt.Sprintf("%s %d", msg, i)
+		time.Sleep(time.Second)
+	}
+}
+
+func Run(args []string) int {
+    if len(args) == 0 {
+	return 0
+    }
+    if len(args) != 2 {
+	if args[0] == "q" {
+	return 0
+    }
+	log.Fatal("Must be invoked with exactly two arguments!\n")
+    }
+
+
     listenStr := args[0]
     firstPeerStr := args[1]
 
     fmt.Printf("kademlia starting up!\n")
     kadem := kademlia.NewKademlia()
-    return
+
     rpc.Register(kadem)
     rpc.HandleHTTP()
     l, err := net.Listen("tcp", listenStr)
     if err != nil {
-        log.Fatal("Listen: ", err)
+	log.Fatal("Listen: ", err)
     }
 
     // Serve forever.
@@ -48,17 +95,19 @@ func main() {
     // printing their results to stdout. See README.txt for more details.
     client, err := rpc.DialHTTP("tcp", firstPeerStr)
     if err != nil {
-        log.Fatal("DialHTTP: ", err)
+	log.Fatal("DialHTTP: ", err)
     }
     ping := new(kademlia.Ping)
     ping.MsgID = kademlia.NewRandomID()
     var pong kademlia.Pong
     err = client.Call("Kademlia.Ping", ping, &pong)
     if err != nil {
-        log.Fatal("Call: ", err)
+	log.Fatal("Call: ", err)
     }
 
     log.Printf("ping msgID: %s\n", ping.MsgID.AsString())
     log.Printf("pong msgID: %s\n", pong.MsgID.AsString())
+    return 1
+
 }
 
