8afea923d9df4c403966c8f07db3655af74c330e
Moritz Gellner <MoritzGellner@Moritzs-MacBook-Pro.local>
2013-02-12 03:46:34 -0600

    fully implemented ping(), added some convenience functions and implemented parts of the Kademlia constructor

--- main/main.go
+++ main/main.go
@@ -12,7 +12,7 @@ import (
     "os"
     "bufio"
     "strings"
-    //"bytes"
+    "strconv"
 )
 
 import (
@@ -20,6 +20,7 @@ import (
 )
 
 var running bool
+var thisNode *kademlia.Kademlia
 
 func main() {
 
@@ -58,8 +59,11 @@ func main() {
 	    	run(arg_s[1],arg_s[2])
 	    } else if arg_s[0] == "ping" && is_cmd_valid(arg_s,1,true) {
 	    	ping(arg_s[1])
-	    } else if arg_s[0] == "store" && is_cmd_valid(arg_s,2,true) {
-	    	store(arg_s[1],arg_s[2])
+	    } else if arg_s[0] == "store" && is_cmd_valid(arg_s,3,true) {
+	    	// k and b are just placeholders for now
+	    	var k kademlia.ID
+	    	var b []byte
+	    	store(k,arg_s[2],b)
 	    } else if arg_s[0] == "find_node" && is_cmd_valid(arg_s,1,true) {
 	    	find_node(arg_s[1])
 	    } else if arg_s[0] == "find_value" && is_cmd_valid(arg_s,1,true) {
@@ -78,10 +82,10 @@ func main() {
 func run(listenStr string, firstPeerStr string) int {
 
     fmt.Printf("Kademlia starting up!\n")
-    kadem := kademlia.NewKademlia()
+    thisNode = kademlia.NewKademlia()
     
     //Register on server
-    rpc.Register(kadem)
+    rpc.Register(thisNode)
     rpc.HandleHTTP()
     l, err := net.Listen("tcp", listenStr)
     if err != nil {
@@ -92,6 +96,21 @@ func run(listenStr string, firstPeerStr string) int {
     go http.Serve(l, nil)
 	running = true
 	
+	
+	/*
+	Add the first contact. For now, just create a new contact with host, port and a random nodeID
+	*/
+	firstContact := new(kademlia.Contact)
+	hostAndPort := strings.Split(firstPeerStr, ":")
+	firstContact.IPAddr = hostAndPort[0]
+	portInt,_ := strconv.Atoi(hostAndPort[1])
+	firstContact.Port = uint16(portInt)
+	firstContact.NodeID = kademlia.NewRandomID()
+	thisNode.AddContactToBuckets(firstContact)
+	// test ping(), both with an ID and a host:port pair as an argument
+	log.Printf("Testing ping() with NodeID\n")
+    ping(firstContact.NodeID.AsString())
+    log.Printf("Testing ping() with host:port\n")
     ping(firstPeerStr)
     return 1
 }
@@ -106,14 +125,32 @@ func is_cmd_valid(cmd []string, argc int, status bool) bool {
 		}
 		return false
 	} else if argc != len(cmd)-1 {
-		log.Printf("Error: Command '%s' must be invoked with %d arguments!\n",cmd[0],len(cmd)-1)
+		log.Printf("Error: Command '%s' must be invoked with %d arguments!\n",cmd[0],argc)
 		return false
 	}
 	return true
 }
 
 func ping(nodeToPing string) int {
-	
+	// if the argument is not of the form host:port, assume that it is a nodeID and 
+	// look up the corresponding host/port pair
+	if len(strings.Split(nodeToPing, ":")) != 2 {
+		id, err := kademlia.FromString(nodeToPing)
+		if err != nil {
+			log.Printf("Error: Could not convert from string to nodeID (%e)\n",err)
+			return 0
+		}
+		contact := kademlia.LookupContact(thisNode,id)
+		if contact == nil {
+			log.Printf("Error: Could not find node with NodeID %s\n",nodeToPing)
+			return 0
+		}
+		hostPort := make([]string, 2)
+		hostPort[0] = contact.IPAddr
+		hostPort[1] = strconv.FormatUint(uint64(contact.Port),10)
+		nodeToPing = strings.Join(hostPort, ":")
+		//log.Printf("Host/Port: %s\n",nodeToPing)
+	}
     client, err := rpc.DialHTTP("tcp", nodeToPing)
     if err != nil {
 	log.Fatal("DialHTTP: ", err)
@@ -128,10 +165,10 @@ func ping(nodeToPing string) int {
 
     log.Printf("ping msgID: %s\n", ping.MsgID.AsString())
     log.Printf("pong msgID: %s\n", pong.MsgID.AsString())
-	return 0
+	return 1
 }
 
-func store(key string, data string) int {
+func store(nodeID kademlia.ID, key string, data []byte) int {
 	return 0
 }
 func find_node(key string) int {
@@ -144,5 +181,27 @@ func get_local_value(key string) int {
 	return 0
 }
 func get_node_id() int {
+	return 0
+}
+
+/*
+Perform the iterativeStore operation and then print the ID of the node that
+received the final STORE operation.
+*/
+func iterativeStore(key string, value []byte) int {
+	return 0
+}
+
+/*
+Print a list of ≤ k closest nodes and print their IDs. You should collect
+the IDs in a slice and print that.
+*/
+func iterativeFindNode(ID kademlia.ID) int { return 0 }
+
+/*
+printf("%v %v\n", ID, value), where ID refers to the node that finally
+returned the value. If you do not find a value, print "ERR".
+*/
+func iterativeFindValue(key string) int {
 	return 0
 }
