29e1ffaa0c98bb4203c66f595c0b0690eaadd860
freedomfita <jacobsherin2008@u.northwestern.edu>
2013-02-16 13:39:25 -0600

    updated dht_testing slightly
    fills each kbucket with 20 contacts

--- kademlia/dht_testing.go
+++ kademlia/dht_testing.go
@@ -1,12 +1,15 @@
 package kademlia
-//***NOT NEARLY FUNCTIONAL
+//only populates the kbuckets randomly, not by distance, more just to check things
 func (k *Kademlia) Add_Random_Nodes(){
-	for i:=0;i<50;i++{
+	L := len(k.K_Buckets)
+	for j:=0;j<L;j++{
+		for i:=0;i<20;i++{
 			c := new(Contact)
 			c.NodeID = NewRandomID()
 			c.IPAddr = "192.168.0.123"
 			c.Port = 7890
 			//b_num := c.NodeID.Xor(k.ThisContact.NodeID).PrefixLen()
-			k.K_Buckets[i][0] = c
+			k.K_Buckets[j][i] = c
+		}
 	}
 }
