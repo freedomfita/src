3d0498dd8d2bf5b3dc881764b27c5b67f4910307
freedomfita <jacobsherin2008@u.northwestern.edu>
2013-02-16 18:31:46 -0600

    Small change to DHT_Testing
    Local_Random_Nodes returns a list of all the IDs of the nodes we
    generated, so we can do store and find node operations

--- kademlia/dht_testing.go
+++ kademlia/dht_testing.go
@@ -13,7 +13,7 @@ func (k *Kademlia) Add_Random_Nodes(){
 		}
 	}
 }
-
+//use MoritzIP for these nodes
 func (k *Kademlia) Random_Nodes() []ID{
 	id_list := make([]ID, 50)
 	for i:=0;i<50;i++{
@@ -21,10 +21,27 @@ func (k *Kademlia) Random_Nodes() []ID{
 		c.NodeID = NewRandomID()
 		id_list[i] = c.NodeID
 		c.IPAddr = "192.168.0.123"
-		c.Port - 7890
+		c.Port = 7890
 		b_num := c.NodeID.Xor(k.ThisContact.NodeID).PrefixLen()
 		k.Next_Open_Spot(b_num)
 		k.K_Buckets[b_num][0] = c
 	}
+	return id_list
+
+}
+//use localhost for these nodes
+func (k *Kademlia) Local_Random_Nodes() []ID{
+	id_list := make([]ID, 50)
+	for i:=0;i<50;i++{
+		c:= new(Contact)
+		c.NodeID = NewRandomID()
+		id_list[i] = c.NodeID
+		c.IPAddr = "192.168.0.123"
+		c.Port = 7890
+		b_num := c.NodeID.Xor(k.ThisContact.NodeID).PrefixLen()
+		k.Next_Open_Spot(b_num)
+		k.K_Buckets[b_num][0] = c
+	}
+	return id_list
 
 }
